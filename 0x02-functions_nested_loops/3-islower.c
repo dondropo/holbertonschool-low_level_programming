1d0136fcce5d32f5bde4dc61c98b04ab347ac0cd
dondropo <alejandroruscamoreno@gmail.com>
2020-02-11 20:04:16 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+  *main 
+  *
+  *Return 1 if c is lowercase
+  */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+{
+	return (1);
+}
+	else
+{
+	return (0);
+}
+}
+
