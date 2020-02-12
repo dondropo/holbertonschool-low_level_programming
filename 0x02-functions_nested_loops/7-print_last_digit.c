c5bd589ae72f57bb58a137e2b4d88e2d4bc400d0
dondropo <alejandroruscamoreno@gmail.com>
2020-02-12 21:19:21 +0000

    last number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,19 @@
 #include "holberton.h"
-int _last_digit(int l)
+/**
+  *print_last_digit - program that prints the last digit of a number
+  *@a: last digit of a number
+  *@l: number to extract the last digit
+  *return - always a
+  */
+int print_last_digit(int l)
 {
-	l = l % 10;
-	_putchar(l);
-	return (0);
+	int a;
+
+	if (l < 0)
+	{
+		l = (l * -1);
+	}
+	a = l % 10;
+	_putchar(a + '0');
+	return (a);
 }
