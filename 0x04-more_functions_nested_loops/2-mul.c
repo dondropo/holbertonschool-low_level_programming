921de6202922cf9273699e71907ce167ce810eae
dondropo <alejandroruscamoreno@gmail.com>
2020-02-13 18:26:07 +0000

    third task (2)

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *_int mul - multiplies 2 integers
+ *@a @b: variables
+ *Return: 0
+ */
+int mul(int a, int b)
+{
+	int mul;
+	mul = (a * b);
+	return (mul);
+}
+
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -2,4 +2,5 @@
 #define holberton
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif 
