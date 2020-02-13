8bc4a903490d32bf548291375428db58392a50cc
dondropo <alejandroruscamoreno@gmail.com>
2020-02-13 18:30:19 +0000

    third task (2) betty ok

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,14 @@
 #include "holberton.h"
 /**
- *_int mul - multiplies 2 integers
- *@a @b: variables
- *Return: 0
+ *mul - multiplies 2 integers
+ *@a: variable
+ *@b: variable
+ *Return: mul
  */
 int mul(int a, int b)
 {
 	int mul;
+
 	mul = (a * b);
 	return (mul);
 }
