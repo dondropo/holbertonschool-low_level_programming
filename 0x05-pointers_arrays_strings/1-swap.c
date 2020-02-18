8841f2f39a565a2dfb83388594ae23781f8d069f
dondropo <alejandroruscamoreno@gmail.com>
2020-02-18 22:14:11 +0000

    task 1, betty checked

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+  *swap_int - swaps the values of two integers
+  *@a: variable to be swapped to 42
+  *@b: variable to be swapped to 98
+  */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
