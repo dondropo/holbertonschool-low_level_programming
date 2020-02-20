8989352913b7b4820401fffc139c917938dbcd06
dondropo <alejandroruscamoreno@gmail.com>
2020-02-20 18:56:42 +0000

    task 0. running and betty checked

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  *_strcat - function that concatenates two strings
+  *@dest: char that returns the concatenated string
+  *@src: char to be concatenated
+  */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	;
+	for (j = 0; src[j] != '\0'; j++, i++)
+		dest[i] = src[j];
+	return (dest);
+}
