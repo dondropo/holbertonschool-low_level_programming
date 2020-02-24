897fff95fe897a0ec711da10026a82bafbad6ee9
dondropo <alejandroruscamoreno@gmail.com>
2020-02-24 19:55:52 +0000

    task 1, betty checked

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
-
+/**
+ *_memcpy - function that copies a memory area
+ *@dest: destination variable of memory space copied
+ *@src: source of memory space to be copied
+ *@n: space to be copied
+ *Return: returns to dest
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
