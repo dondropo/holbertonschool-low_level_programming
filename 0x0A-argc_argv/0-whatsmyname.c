e9856bde44072cc0ac7ddd2f2067bece3e252feb
dondropo <alejandroruscamoreno@gmail.com>
2020-03-02 03:35:21 +0000

    upload of task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - first declaration
+ * @argc: number of arguments
+ * @argv: array that contains the arguments
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)),
+	char *argv[] __attribute__((unused)))
+{
+	printf("%s\n", *argv);
+	return  (0);
+}
