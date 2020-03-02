252ed95d2468e25ea342e182fba4c4189827f131
dondropo <alejandroruscamoreno@gmail.com>
2020-03-02 04:00:26 +0000

    upload of task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - init funct.
+ * @argc: number of arguments.
+ * @argv: arguments pessed
+ * Return: returns 0.
+*/
+int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
