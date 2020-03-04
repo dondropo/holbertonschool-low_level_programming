0b14b2e0c4867a64651893392f082bf35f952d63
dondropo <alejandroruscamoreno@gmail.com>
2020-03-04 03:17:03 +0000

    add task 0, betty checked and running

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,11 @@
 #include "holberton.h"
 #include <stdlib.h>
-
+/**
+ *create_array - creates an array initialized with a specific char
+ *@size: array to be created
+ *@c: value to be inserted
+ *Return: returns final value of array
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *array;
