1cce9ea4700b5bfba06170498f0741d8db1e3abd
dondropo <alejandroruscamoreno@gmail.com>
2020-03-04 05:03:25 +0000

    add task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * free_grid - frees 2 dimensional grids
+ * @grid: double point
+ * @height: var
+ * Return: none
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
