19c209066458bbe27643bae8154e103723617e42
dondropo <alejandroruscamoreno@gmail.com>
2020-03-11 17:12:06 +0000

    add task 0, betty checked

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,9 @@
 #include "function_pointers.h"
-
+/**
+ *print_name - function that prints a name
+ *@name: string of type char to be printed
+ *@f: pointed function to print the name
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (f == NULL)
