a47116a5758d98c85771ed20f537faa35750e021
dondropo <alejandroruscamoreno@gmail.com>
2020-03-11 03:37:51 +0000

    try

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,9 +13,11 @@ struct dog
 	char *owner;
 };
 
+int _strlen(char *s);
+char *_memcpy(char *dest, char *src, unsigned int n);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-/**void free_dog(dog_t *d);*/
+/*void free_dog(dog_t *d);*/
 
 #endif
