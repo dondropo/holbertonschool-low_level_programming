09a1dafd03968d1a7b29d19bcc41975a4976d494
dondropo <alejandroruscamoreno@gmail.com>
2020-03-14 22:24:13 +0000

    add library with tasks 0,1,2 and 3 prototypes and 3's struct declared

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,6 +8,13 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+
+typedef struct va
+{
+	char *check_type;
+	void (*fun)(va_list list);
+} va_f;
+
+void print_all(const char * const format, ...); /*task 3 prototype*/
 
 #endif
