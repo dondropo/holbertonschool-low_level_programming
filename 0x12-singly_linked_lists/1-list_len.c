534e9b6a34fae41aedd19bcd56ff23daace0f7f3
dondropo <alejandroruscamoreno@gmail.com>
2020-03-19 23:53:01 +0000

    adding task 1, betty checked

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *list_len - funct that returns the number
+ *@h: node refferencing struct
+ *Return: returns the counters value
+ */
+size_t list_len(const list_t *h)
+
+{
+	size_t linked_size = 0;
+
+	while (h != NULL)
+	{
+		linked_size++;
+		h = h->next;
+
+	}
+	return (linked_size);
+}
