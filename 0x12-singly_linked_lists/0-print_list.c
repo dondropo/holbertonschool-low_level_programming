3d121b0eadb6604a5631e92096a47a70222556c8
dondropo <alejandroruscamoreno@gmail.com>
2020-03-19 22:19:57 +0000

    adding task 0, betty checked

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ *print_list - prints all elements of a list_t list
+ *@h: nod refferencing the struct
+ *
+ *Return: returns the counter value
+ */
+size_t print_list(const list_t *h)
+{
+	int counter = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			counter++;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+			counter++;
+		}
+	}
+	return (counter);
+}
