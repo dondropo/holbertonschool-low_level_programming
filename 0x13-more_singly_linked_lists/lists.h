942c5180821eeea1157d9772263b05df0741193b
dondropo <alejandroruscamoreno@gmail.com>
2020-03-24 15:50:43 +0000

    update library

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
-#ifndef "LISTS"
-#define "LISTS"
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list
@@ -11,21 +15,22 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
+
 } listint_t;
 
 int _putchar(char c);
-size_t print_listint(const listint_t *h);/*task 0 proto*/
-size_t listint_len(const listint_t *h);/*task 1 proto*/
-listint_t *add_nodeint(listint_t **head, const int n);/*task 2 proto*/
-listint_t *add_nodeint_end(listint_t **head, const int n);/*task 3 proto*/
-void free_listint(listint_t *head);/*task 4 proto*/
-void free_listint2(listint_t **head);/*task 5 proto*/
-int pop_listint(listint_t **head);/*task 6 proto*/
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);/*task 7 proto*/
-int sum_listint(listint_t *head);/*task 8 proto*/
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);/*task 9*/
-int delete_nodeint_at_index(listint_t **head, unsigned int index);/*task 10*/
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 #endif
