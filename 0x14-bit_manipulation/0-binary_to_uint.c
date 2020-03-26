fd4cd96fca8c3ac5dca8345f08396ac71fd74e74
dondropo <alejandroruscamoreno@gmail.com>
2020-03-26 22:30:52 +0000

    adding task 1, working

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,17 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0;
 
-	if (!*b && b == NULL)
+	if (b == NULL)
 		return (0);
 
-	while (*b)
+	while (*b != '\0')
 	{
-		n <<= 1;
-		if (*b == '1')
-			n += 1;
-		else if  (*b != '0' && *b != '1')
+		n = n << 1;
+		if (*b != '0' && *b != '1')
 			return (0);
-		++b;
+		else if  (*b == '1')
+			n = n | 1;
+		b++;
 	}
 	return (n);
 }
