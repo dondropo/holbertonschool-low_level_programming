8fbbee323eaa9e76524670e798408a28c6c43e94
dondropo <alejandroruscamoreno@gmail.com>
2020-04-01 01:33:19 +0000

    comprobation fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,14 +13,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		while (*(text_content + txt_len))
-			txt_len++;
-		file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	}
-	else
+	if (text_content == NULL)
 		text_content = "";
+
+	while (*(text_content + txt_len))
+	{
+			txt_len++;
+			file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	}
 
 	if (file_d == -1)
 		return (-1);
