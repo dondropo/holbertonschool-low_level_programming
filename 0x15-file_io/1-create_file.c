124054a1836db1cb9c47c23d7119a108db8c9789
dondropo <alejandroruscamoreno@gmail.com>
2020-04-01 02:45:30 +0000

    adding task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,17 +13,17 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content == NULL)
+		text_content = "";
+
+	else
 	{
 		while (*(text_content + txt_len))
 		{
 			txt_len++;
 		}
+		file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	}
-	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (text_content == NULL)
-		text_content = "";
 
 	if (file_d == -1)
 		return (-1);
