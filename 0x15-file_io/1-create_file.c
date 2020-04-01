8c6990e0394ca57fafdc88c4ba0678768117c753
dondropo <alejandroruscamoreno@gmail.com>
2020-04-01 03:45:20 +0000

    adding task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,25 +4,24 @@
  *@filename: name of the file to be created
  *@text_content: text content of the new file
  *
- *Return: returns 1 (succes) or -1 (fail)
+ *Return: returns 1 (success) or -1 (fail)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_d, txt_len = 0;
+	int file_d;
+	int txt_len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		while (*(text_content + txt_len))
-		{
-			txt_len++;
-		}
-		file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	}
-	else
+	if (text_content == NULL)
 		text_content = "";
+
+	while (*(text_content + txt_len))
+	{
+		txt_len++;
+	}
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file_d == -1)
 		return (-1);
