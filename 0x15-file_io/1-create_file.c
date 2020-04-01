fc6d8dc9ce9c78eaba76a852917166365dfb1151
dondropo <alejandroruscamoreno@gmail.com>
2020-04-01 01:28:24 +0000

    adding task 1, working and betty checked

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ *create_file - function that creates a file
+ *@filename: name of the file to be created
+ *@text_content: text content of the new file
+ *
+ *Return: returns 1 (succes) or -1 (fail)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_d, txt_len;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (*(text_content + txt_len))
+			txt_len++;
+		file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	}
+	else
+		text_content = "";
+
+	if (file_d == -1)
+		return (-1);
+
+	write(file_d, text_content, txt_len);
+	close(file_d);
+
+	return (1);
+
+}
