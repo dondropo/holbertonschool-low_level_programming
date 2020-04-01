0775958b14020f7f10374489040bbf09025bd3e1
dondropo <alejandroruscamoreno@gmail.com>
2020-04-01 02:38:29 +0000

    task 1, fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,8 +19,9 @@ int create_file(const char *filename, char *text_content)
 		{
 			txt_len++;
 		}
-	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	}
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (text_content == NULL)
 		text_content = "";
 
