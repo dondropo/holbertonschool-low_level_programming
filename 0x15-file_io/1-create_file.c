383f242eced861a1e0e0f1559a4588ff004c88ea
dondropo <alejandroruscamoreno@gmail.com>
2020-04-01 02:19:20 +0000

    adding file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 			file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 		}
 	}
-	else
+	if (text_content == NULL)
 		text_content = "";
 
 	if (file_d == -1)
