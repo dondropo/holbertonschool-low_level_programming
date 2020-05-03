317b59af1c0f5e566551b1aaa51f3b15d13a959f
dondropo <alejandroruscamoreno@gmail.com>
2020-05-03 18:18:01 +0000

    Task 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ *get_dnodeint_at_index - Adds a node at the index of a dlistint_t list
+ *@head: pointer to head of the list
+ *@index: index of the node to be got
+ *
+ *Return: node_index in succes or NULL if failed.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int counter = 0;
+
+	dlistint_t *node_index = head;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (counter < index)
+		{
+			node_index = head->next;
+			head = node_index;
+			counter++;
+		}
+	}
+	return (node_index);
+}
