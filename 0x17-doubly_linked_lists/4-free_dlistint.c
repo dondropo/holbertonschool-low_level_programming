ef1276cceebbcfb508a42e9803d9645d255e3d76
dondropo <alejandroruscamoreno@gmail.com>
2020-05-03 06:35:52 +0000

    Task 4, check for fix?

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ *free_dlistint - frees a dlistint_t list
+ *@head: pointer to the list head
+ *
+ *Return: none
+ */
+void free_dlistint(dlistint_t *head)
+{
+	while (head != NULL)
+	{
+		head = head->next;
+		free(head);
+	}
+}
