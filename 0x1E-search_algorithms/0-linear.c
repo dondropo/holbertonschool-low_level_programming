7a93670fba36ccdf5c323cb1a04f11ff6566b34e
dondropo <alejandroruscamoreno@gmail.com>
2020-08-26 17:08:48 +0000

    [Add files]: the right file

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+* linear_search - Find value element into "array" array
+* using linear search
+*
+* @array: pointer to the array
+* @size: size of the array
+* @value: the value to be found
+*
+* Return: index in succes. -1 if fails
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t index;
+
+	for (index = 0; index <= size; index++)
+	{
+		printf("Value checked array [%li] = [%d]\n", index, array[index]);
+		if (array[index] == value)
+			return (index);
+	}
+	return (-1);
+}
