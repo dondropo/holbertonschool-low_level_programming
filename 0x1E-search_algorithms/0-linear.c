3fb01c82528d1f20138b243da472bb0ca6501d9d
dondropo <alejandroruscamoreno@gmail.com>
2020-08-26 17:39:50 +0000

    [Fix]

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t index;
 
-	if (array == NULL || !value)
+	if (array == NULL)
 		return (-1);
 
 	for (index = 0; index <= size; index++)
