f4eecd97228678bf45056a07eee7eb3c4443869d
dondropo <alejandroruscamoreno@gmail.com>
2020-08-26 17:11:59 +0000

    [Fix]: returning null if the array is empty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,6 +14,9 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t index;
 
+	if (array == NULL)
+		return (-1)
+
 	for (index = 0; index <= size; index++)
 	{
 		printf("Value checked array [%li] = [%d]\n", index, array[index]);
